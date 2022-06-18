564e1006152f8620cc98d07abd7a764d6bc76976
Andrey Etumyan <andreyetumyan@gmail.com>
2022-06-18 22:58:33 +0300

    StoryListComponent overall refactoring

--- app/src/app/sidebar/story-list.component.ts
+++ app/src/app/sidebar/story-list.component.ts
@@ -4,13 +4,13 @@ import { Router } from '@angular/router';
 import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatTreeNestedDataSource } from '@angular/material/tree';
 
-import { stories } from '../../stories';
-import { parsePath } from '../utils/parse-path';
+import { stories as storyFiles } from '../../stories';
+import { trim } from '../utils/trim';
 
 interface TreeNode {
   name: string;
+  path: string[];
   children?: TreeNode[];
-  path: ['components', 'button', 'primary'];
 }
 
 @Component({
@@ -20,52 +20,75 @@ interface TreeNode {
   providers: [Location, { provide: LocationStrategy, useClass: PathLocationStrategy }],
 })
 export class StoryListComponent {
-  storiesTree: any[];
-
   treeControl = new NestedTreeControl<TreeNode>(node => node.children);
 
   dataSource = new MatTreeNestedDataSource<TreeNode>();
 
-  selectedStory = ['components', 'button', 'primary'];
+  constructor(private location: Location, private router: Router) {
+    this.dataSource.data = this.generateTree();
 
-  hasChild = (_: number, node: TreeNode) => !!node.children && node.children.length > 0;
+    const selectedNodePath = trim(this.location.path(), '/');
+    const selectedNodeParentPath = selectedNodePath.split('/').slice(0, -1);
+    const selectedNodeParent = this.getNodeByPath(this.dataSource.data, selectedNodeParentPath);
 
-  constructor(private router: Router, private location: Location) {
-    this.storiesTree = this.generateTree();
-    this.dataSource.data = this.storiesTree;
-
-    const parsedPath = parsePath(location.path());
-    const selectedNode = this.dataSource.data.find(item => item.name === parsedPath.component);
-
-    if (selectedNode) {
-      this.treeControl.expand(selectedNode);
+    if (selectedNodeParent) {
+      this.treeControl.expand(selectedNodeParent);
     }
   }
 
-  select(path: any) {
+  isNodeHasChildren(_: number, node: TreeNode) {
+    return node.children && node.children.length;
+  }
+
+  isNodeSelected(nodePath: string[]) {
+    const selectedNodePath = trim(this.location.path(), '/');
+
+    return nodePath.join('/') === selectedNodePath;
+  }
+
+  selectNode(path: string[]) {
     this.router.navigate(path);
   }
 
-  isSelected(path: string[]) {
-    if (path.join('/') === this.location.path().replace(/^\//, '')) {
-      return true;
-    }
-
-    return false;
-  }
-
   private generateTree() {
-    return Object.keys(stories).map(key => {
-      const component = (stories as any)[key];
-      const _stories = Object.keys(component).filter(key => key !== 'default');
-      const titleElements = component.default.title.split('/');
+    return Object.keys(storyFiles).map(storyFileKey => {
+      const { metadata, stories } = this.normalizeStoryFile((storyFiles as any)[storyFileKey]);
 
       return {
-        test: 1,
-        name: titleElements[titleElements.length - 1],
-        children: _stories.map(key => ({ name: key, path: [...titleElements, key] })),
-        path: titleElements,
+        name: metadata.name,
+        path: metadata.path,
+        children: stories.map(name => ({
+          name,
+          path: [...metadata.path, name],
+        })),
       };
     });
+  }
+
+  private getNodeByPath(tree: TreeNode[], path: string[]) {
+    const queue = [...tree];
+
+    while (queue.length) {
+      const item = queue.shift()!;
+      if (item.path.join('/') === path.join('/')) {
+        return item;
+      } else {
+        item.children && queue.push(...item.children);
+      }
+    }
+
+    return;
+  }
+
+  private normalizeStoryFile(storyFile: any) {
+    const title = storyFile.default.title.split('/');
+
+    return {
+      metadata: {
+        name: title[title.length - 1],
+        path: title,
+      },
+      stories: Object.keys(storyFile).filter(key => key !== 'default'),
+    }
   }
 }
