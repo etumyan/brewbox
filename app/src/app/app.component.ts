93de3626568837c7e86002787db20aea29aabe3c
Andrey Etumyan <andreyetumyan@gmail.com>
2022-06-18 23:12:19 +0300

    change page title based on path

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Location } from '@angular/common';
+
+import { trim } from './utils/trim';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private titleService: Title, private location: Location) {}
+
+  ngOnInit() {
+    this.location.onUrlChange(url => {
+      this.titleService.setTitle(
+        this.generateTitleFromPath(url),
+      );
+    });
+  }
+
+  private generateTitleFromPath(path: string) {
+    return trim(path, '/').split('/').join(' / ') || 'Brewbox';
+  }
+}
